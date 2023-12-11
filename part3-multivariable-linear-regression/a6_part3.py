33fa115cb1ea8cc451d2a9c2b93e512708706f40
Will Otwell <90997466+wo4455@users.noreply.github.com>
2023-12-10 19:22:40 -0600

    upload

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,30 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
-
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {intercept}")
+print("R Squared value:", r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
 print("***************")
 print("Testing Results")
+
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+print(predict)
+
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index]
+    print(f"Miles: {x_coord[0]} Age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,13 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
+The r squred coefficient is ~0.8 and shows how well the model fits the trend of the data. 
 
 2. Is your model accurate? Why or why not?
+I think that this model is moderately accurate because the perdictions are close to the actual values and the r squared value is decently high. 
 
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
+The first prediction is 8703.25, and the second prediction is 714.10. 
 
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+If the input value gets to a certain point then it will be negative because it's decreasing. 
